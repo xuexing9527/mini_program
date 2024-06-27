44e22aba30d4e50d30d3f0988416c6b8f31ca5b7
fn <xuexing9527@163.com>
2024-06-27 17:12:21 +0800

    init

---
+++ miniprogram/components/banner/index.js
@@ -0,0 +1,84 @@
+// layout/index.js
+
+const fruit = [
+  'https://wx3.sinaimg.cn/large/73440aa6ly1h062q1h1wtj20m80ci3z5.jpg',
+  'https://img.d1cm.com/news/img/202212201055101370.jpg',
+  'https://xqimg.imedao.com/18f6fa2a1b33abbe3fd5204d.jpg%21800.jpg'
+  // 'https://img0.baidu.com/it/u=2976099989,2579072426&fm=253&fmt=auto&app=138&f=JPEG?w=889&h=500',
+  // 'https://img2.baidu.com/it/u=539605762,1499341472&fm=253&fmt=auto&app=120&f=JPEG?w=1422&h=800',
+  // 'https://img2.baidu.com/it/u=1988305640,2153018468&fm=253&fmt=auto&app=138&f=JPEG?w=889&h=500',
+  // 'https://img2.baidu.com/it/u=1389301813,964921417&fm=253&fmt=auto&app=120&f=JPEG?w=750&h=450',
+]
+
+const transformers = [
+  'accordion',
+  'threeD',
+  'zoomIn',
+  'zoomOut',
+  'deepthPage'
+]
+
+Page({
+    /**
+     * 页面的初始数据
+     */
+    data: {
+      fruit,
+    },
+
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad(options) {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面初次渲染完成
+     */
+    onReady() {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面显示
+     */
+    onShow() {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面隐藏
+     */
+    onHide() {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面卸载
+     */
+    onUnload() {
+
+    },
+
+    /**
+     * 页面相关事件处理函数--监听用户下拉动作
+     */
+    onPullDownRefresh() {
+
+    },
+
+    /**
+     * 页面上拉触底事件的处理函数
+     */
+    onReachBottom() {
+
+    },
+
+    /**
+     * 用户点击右上角分享
+     */
+    onShareAppMessage() {
+
+    }
+})
